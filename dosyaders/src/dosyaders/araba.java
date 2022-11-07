0f26816dfc8338b26529ef89bd0461175a74200f
YunusEmreCinbolat <99325996+YunusEmreCinbolat@users.noreply.github.com>
2022-11-07 22:06:01 +0300

    Add files via upload

---
+++ dosyaders/src/dosyaders/araba.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dosyaders;
+
+/**
+ *
+ * @author user
+ */
+public class araba {
+    String model;
+    int beygir;
+    String renk;
+
+    public araba() {
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getBeygir() {
+        return beygir;
+    }
+
+    public void setBeygir(int beygir) {
+        this.beygir = beygir;
+    }
+
+    public String getRenk() {
+        return renk;
+    }
+
+    public void setRenk(String renk) {
+        this.renk = renk;
+    }
+    
+}
